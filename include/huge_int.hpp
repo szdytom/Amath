63142398fc44553460ce8f49f586da3467c60ae3
zhangtianli2006 <49156174+zhangtianli2006@users.noreply.github.com>
2020-07-20 15:27:58 +0800

    Fix format

--- include/huge_int.hpp
+++ include/huge_int.hpp
@@ -2,18 +2,15 @@
 #include <string>
 
 // Huge_int by ZTL
-class huge_int
-{
-private:
+class huge_int {
+    private:
     std::string bits;
     bool positive;
-    inline void flip(std::string &str)
-    {
+    inline void flip(std::string &str) {
         for (int i = 0; i < str.size() / 2; i++)
             std::swap(str[i], str[str.size() - i - 1]);
     }
-    inline bool bigger(std::string a, std::string b)
-    {
+    inline bool bigger(std::string a, std::string b) {
         if (a.size() != b.size())
             return a.size() > b.size();
         for (int i = 0; i < a.size(); i++)
@@ -21,8 +18,7 @@ private:
                 return a[i] > b[i];
         return false;
     }
-    inline bool smaller(std::string a, std::string b)
-    {
+    inline bool smaller(std::string a, std::string b) {
         if (a.size() != b.size())
             return a.size() < b.size();
         for (int i = 0; i < a.size(); i++)
@@ -31,8 +27,7 @@ private:
         return false;
     }
     // Add
-    inline std::string add(std::string a, std::string b)
-    {
+    inline std::string add(std::string a, std::string b) {
         if (b.size() > a.size())
             swap(a, b);
         std::string c = a;
@@ -41,8 +36,7 @@ private:
         flip(a);
         flip(b);
         int carry = 0;
-        for (int i = 0; i < a.size(); i++)
-        {
+        for (int i = 0; i < a.size(); i++) {
             int tmp = a[i] - '0' + b[i] - '0' + carry;
             c[i] = (tmp % 10) + '0';
             carry = tmp / 10;
@@ -53,8 +47,7 @@ private:
         return c;
     }
     // Difference
-    inline std::string diff(std::string a, std::string b)
-    {
+    inline std::string diff(std::string a, std::string b) {
         if (bigger(b, a))
             swap(a, b);
         std::string c = a;
@@ -63,8 +56,7 @@ private:
         flip(a);
         flip(b);
         int carry = 0;
-        for (int i = 0; i < a.size(); i++)
-        {
+        for (int i = 0; i < a.size(); i++) {
             int tmp = a[i] - b[i] - carry;
             a[i] += (tmp < 0 ? 10 : 0);
             c[i] = a[i] - b[i] - carry + '0';
@@ -80,42 +72,34 @@ private:
         return c;
     }
 
-public:
-    huge_int()
-    {
+    public:
+    huge_int() {
         positive = true;
     }
-    ~huge_int()
-    {
+    ~huge_int() {
         this->bits.clear();
         this->bits.shrink_to_fit();
         this->positive = true;
     }
     // Get huge_int's lenth
-    inline int size()
-    {
+    inline int size() {
         return bits.size();
     }
     // Clear huge_int
-    inline void clear()
-    {
+    inline void clear() {
         this->bits.clear();
         this->bits.shrink_to_fit();
         this->positive = true;
     }
     // Convert int to huge_int
-    inline void import_int(int x)
-    {
+    inline void import_int(int x) {
         huge_int res;
-        if (x < 0)
-        {
+        if (x < 0) {
             this->positive = false;
             x = -x;
-        }
-        else
+        } else
             this->positive = true;
-        while (x)
-        {
+        while (x) {
             std::string tmp = "#";
             tmp[0] = x % 10 + '0';
             this->bits = tmp + this->bits;
@@ -123,18 +107,14 @@ public:
         }
     }
     // Convert long long to huge_int
-    inline void import_long_long(long long x)
-    {
+    inline void import_long_long(long long x) {
         huge_int res;
-        if (x < 0)
-        {
+        if (x < 0) {
             this->positive = false;
             x = -x;
-        }
-        else
+        } else
             this->positive = true;
-        while (x)
-        {
+        while (x) {
             std::string tmp = "#";
             tmp[0] = x % 10 + '0';
             this->bits = tmp + this->bits;
@@ -142,12 +122,10 @@ public:
         }
     }
     // Operator >
-    inline bool operator>(const huge_int x)
-    {
+    inline bool operator>(const huge_int x) {
         if (this->positive != x.positive)
             return this->positive > x.positive;
-        else
-        {
+        else {
             if (this->positive == 1)
                 return bigger(this->bits, x.bits);
             else
@@ -156,12 +134,10 @@ public:
         return false;
     }
     // Operator <
-    inline bool operator<(const huge_int x)
-    {
+    inline bool operator<(const huge_int x) {
         if (this->positive != x.positive)
             return this->positive < x.positive;
-        else
-        {
+        else {
             if (this->positive == 1)
                 return smaller(this->bits, x.bits);
             else
@@ -170,8 +146,7 @@ public:
         return false;
     }
     // Operator ==
-    inline bool operator==(const huge_int x)
-    {
+    inline bool operator==(const huge_int x) {
         if (*this < x)
             return false;
         if (*this > x)
@@ -179,24 +154,20 @@ public:
         return true;
     }
     // Operator !=
-    inline bool operator!=(const huge_int x)
-    {
+    inline bool operator!=(const huge_int x) {
         if (*this == x)
             return false;
         return true;
     }
     // Operator =
-    inline huge_int &operator=(huge_int x)
-    {
+    inline huge_int &operator=(huge_int x) {
         this->bits = x.bits;
         this->positive = x.positive;
         return *this;
     }
     // Operator []
-    inline int operator[](const int x)
-    {
-        if (x >= this->bits.size())
-        {
+    inline int operator[](const int x) {
+        if (x >= this->bits.size()) {
             std::cerr << "huge_int:\n";
             std::cerr << "int &Operator[int x]\n";
             std::cerr << "              ^~~~~\n";
@@ -206,10 +177,8 @@ public:
         return this->bits[x] - '0';
     }
     // Convert huge_int to int
-    inline int export_int()
-    {
-        if (bigger(this->bits, "2147483647"))
-        {
+    inline int export_int() {
+        if (bigger(this->bits, "2147483647")) {
             std::cerr << "huge_int:\n";
             std::cerr << "int export_int()\n";
             std::cerr << "*this > INT_MAX or *this < INT_MIN\n";
@@ -217,16 +186,14 @@ public:
         }
         int ans = 0;
         int bit = 1;
-        for (int i = this->size()  - 1; i >= 0; i--)
-        {
+        for (int i = this->size() - 1; i >= 0; i--) {
             ans += (*this)[i] * bit;
             bit *= 10;
         }
         return ans;
     }
     // Operator <<
-    friend std::ostream &operator<<(std::ostream &os, const huge_int &x)
-    {
+    friend std::ostream &operator<<(std::ostream &os, const huge_int &x) {
         if (x.positive == false)
             os << '-' << x.bits;
         else
@@ -234,46 +201,35 @@ public:
         return os;
     }
     // Operator >>
-    friend std::istream &operator>>(std::istream &is, huge_int &x)
-    {
+    friend std::istream &operator>>(std::istream &is, huge_int &x) {
         is >> x.bits;
         x.positive = true;
-        if (x.bits[0] == '-')
-        {
+        if (x.bits[0] == '-') {
             x.bits = x.bits.substr(1, x.bits.size() - 1);
             x.positive = false;
-        }
-        else if (x.bits[0] == '+')
-        {
+        } else if (x.bits[0] == '+') {
             x.bits = x.bits.substr(1, x.bits.size() - 1);
             x.positive = true;
         }
         return is;
     }
     // Operator +
-    inline huge_int operator+(const huge_int x)
-    {
+    inline huge_int operator+(const huge_int x) {
         huge_int res;
-        if (this->positive == x.positive)
-        {
+        if (this->positive == x.positive) {
             res.bits = add(this->bits, x.bits);
             if (this->positive == 1)
                 res.positive = true;
             else
                 res.positive = false;
-        }
-        else
-        {
+        } else {
             res.bits = diff(this->bits, x.bits);
-            if (this->positive == 1)
-            {
+            if (this->positive == 1) {
                 if (smaller(this->bits, x.bits))
                     res.positive = false;
                 else
                     res.positive = true;
-            }
-            else
-            {
+            } else {
                 if (bigger(this->bits, x.bits))
                     res.positive = false;
                 else
@@ -283,29 +239,22 @@ public:
         return res;
     }
     // Operator -
-    inline huge_int operator-(const huge_int x)
-    {
+    inline huge_int operator-(const huge_int x) {
         huge_int res;
-        if (this->positive == x.positive)
-        {
+        if (this->positive == x.positive) {
             res.bits = diff(this->bits, x.bits);
-            if (this->positive == 1)
-            {
+            if (this->positive == 1) {
                 if (smaller(this->bits, x.bits))
                     res.positive = false;
                 else
                     res.positive = true;
-            }
-            else
-            {
+            } else {
                 if (bigger(this->bits, x.bits))
                     res.positive = false;
                 else
                     res.positive = true;
             }
-        }
-        else
-        {
+        } else {
             res.bits = add(this->bits, x.bits);
             if (this->positive == 1)
                 res.positive = true;
@@ -315,13 +264,11 @@ public:
         return res;
     }
     // Operator +=
-    inline void operator+=(huge_int x)
-    {
+    inline void operator+=(huge_int x) {
         *this = *this + x;
     }
     // Operator -=
-    inline void operator-=(huge_int x)
-    {
+    inline void operator-=(huge_int x) {
         *this = *this - x;
     }
 };
