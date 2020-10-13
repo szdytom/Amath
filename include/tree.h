07189ea5b98a3f614fd8505d96162373836c76b9
zhangtianli2006 <zhangtianli2006@163.com>
2020-10-13 13:52:25 +0800

    add lca

--- include/tree.h
+++ include/tree.h
@@ -4,27 +4,28 @@
 #include <map>
 #include <vector>
 
-namespace amath {
-
-template <typename T>
-class tree_node {
-public:
-    void push_back(T __son);
-    size_t size();
-
-private:
-    std::vector<T> son_v;
-};
+namespace algo {
 
 template <typename T>
 class tree {
 public:
-    tree_node<T> operator[](const size_t __index);
+    T& operator[](const size_t __index);
+    void new_son(const size_t __father_index, const size_t __son_index);
+
+    size_t lca(size_t __node_a, size_t __node_b, const size_t __root);
 
 private:
-    std::map<size_t, tree_node<T>> tree_map;
+    void lca_dfs(size_t u);
+
+    const size_t SUPER_ROOT = SIZE_MAX;
+    std::map<size_t, std::vector<size_t>> son;
+    std::map<size_t, size_t> father;
+    std::map<size_t, T> value;
+
+    std::map<std::pair<size_t, unsigned int>, size_t> lca_father;
+    std::map<size_t, size_t> lca_depth;
 };
 
-} // namespace amath
+} // namespace algo
 
 #endif
